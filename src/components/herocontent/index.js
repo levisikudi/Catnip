112cad8f9c99ae3343fe4edde6f1d3f42fa05f3e
Levis Sikudi <levisikudi@gmail.com>
2023-03-09 06:38:57 -0500

    Guest Login Implemented

--- src/components/herocontent/index.js
+++ src/components/herocontent/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { loginUser } from '../../utilities/userUtilities'
 
 
 const HeroContent = (props) => {
@@ -10,12 +11,29 @@ const HeroContent = (props) => {
     setShowLogin(true)
   }
 
+
+   
+      const handleGuestLogin = async () => {
+        await loginUser({email: "eli@sch", password: "helpless"});
+        // get session info (user)
+        let user = await getUserfromSession()
+        setUser(user);
+        nav('/user/dash')
+
+      }
+      
+
+      
+   
+  
+
   return (
     <>
     <p className='display-1 mb-5 '>Hello there</p>
           <div className='row mt-3 gap-2 '>
             <button className='btn btn-outline-success text-white col-12' onClick={handleButtonClick}>Login</button>
-            <button className='btn btn-success text-white col-12'>Login with Guest Credentials</button>
+            <button className='btn btn-success text-white col-12'
+            onClick={handleGuestLogin}>Login with Guest Credentials</button>
           </div>
     </>
   )
